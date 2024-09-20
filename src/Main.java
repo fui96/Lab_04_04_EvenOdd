d597e1d800056069e969a2339b8c98b5487d3d9e
fui96 <kidwelqn@mail.uc.edu>
2024-09-20 16:51:05 -0400

    innit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int UserNum = 37;
+        if ((UserNum % 2) > 0) {
+            System.out.println("The number is odd");
+        }
+        else{
+            System.out.println("The number is even");
+        }
+        }
+    }
